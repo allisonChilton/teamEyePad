3d05fc8590db96c1a6b037a9fbed2e150279bcec
Ash Srinivas <ash.srinivas@gmail.com>
2015-11-13 01:03:38 -0600

    Added EyeX library DLLs and Setup the App's EyeX Host

--- EyePad/EyePad.UI/App.xaml.cs
+++ EyePad/EyePad.UI/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using EyeXFramework.Wpf;
 
 namespace EyePad.UI
 {
@@ -13,5 +14,25 @@ namespace EyePad.UI
     /// </summary>
     public partial class App : Application
     {
+        // Maintain a reference to the Tobii EyeX Host
+        // object in the application so that it isn't
+        // garbage collected by the CLR while the app
+        // is still running
+        private WpfEyeXHost _eyeXHost;
+
+        public App()
+        {
+            _eyeXHost = new WpfEyeXHost();
+            _eyeXHost.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            // Dispose of the application's WpfEyeXHost
+            // object upon exitting the application
+            _eyeXHost.Dispose();
+        }
     }
 }
